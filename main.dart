983ce6efee0cb8311983c221f49851d29a39763d
saad480280 <sk1338079@gmail.com>
2024-08-17 10:43:44 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,39 +1,31 @@
-void main() {
-  String txt = "CS6";
-  if (txt == "CS6") {
-    print('hello' + " " + txt);
-  } else {
-    print("no hello");
-  }
-}
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         body: Container(
-//           decoration: const BoxDecoration(
-//             gradient: LinearGradient(
-//               colors: [
-//                 Color.fromRGBO(226, 99, 60, 1),
-//                 Color.fromRGBO(109, 37, 8, 1),
-//               ],
-//               begin: Alignment.topLeft,
-//               end: Alignment.bottomRight,
-//             ),
-//           ),
-//           child: const Center(
-//             child: Text(
-//               'Saad khan 1934',
-//               style: TextStyle(
-//                 color: Colors.blueAccent,
-//                 fontSize: 28,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(226, 99, 60, 1),
+                Color.fromRGBO(109, 37, 8, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Saad khan 1934',
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
